20686eca1d6ada67dc90691561d530a0747bca25
Kevin Lucas <66319272+kevin504-max@users.noreply.github.com>
2023-05-02 23:24:45 -0300

    Login API Integration

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -4,12 +4,31 @@
 <div class="login">
     <input type="text" v-model="email" placeholder="Enter Email">
     <input type="password" v-model="password" placeholder="Enter Password">
-    <button>Login</button>
+    <button v-on:click="login">Login</button>
     <p><router-link to="/sign-up">Sign Up</router-link></p>
 </div></template>
 
 <script>
+import axios from "axios";
+
 export default {
-    name: "LoginPage"
+    name: "LoginPage",
+    data() {
+        return {
+            email: "",
+            password: ""
+        }
+    },
+    methods: {
+        async login() {
+            let result = await axios.get(`http://localhost:3000/users?email=${this.email}&password=${this.password}`)
+            console.warn(result);
+
+            if(result.status == 200 && result.data.lenght > 0 ) {
+                localStorage.setItem("user-info", JSON.stringify(result.data[0]));
+                this.$router.push({name: "HomePage"});
+            }
+        }
+    }
 }
 </script>

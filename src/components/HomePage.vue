55f084c2c9c49d720633c3fedec56e9fa6abf079
Kevin Lucas <66319272+kevin504-max@users.noreply.github.com>
2023-05-03 00:19:23 -0300

    List of restaurant with API

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,30 +1,55 @@
 <template>
 <HeaderComponent />
 <h1>Hi {{ name }}, Welcome on Home Page!</h1>
+<table border="1">
+    <tr>
+        <td>ID</td>
+        <td>Name</td>
+        <td>Contact</td>
+        <td>Address</td>
+    </tr>
+    <tr v-for="item in restaurant" :key="item.id">
+        <td>{{ item.id }}</td>
+        <td>{{ item.name }}</td>
+        <td>{{ item.contact }}</td>
+        <td>{{ item.address }}</td>
+    </tr>
+</table>
 </template>
 
 <script>
 import HeaderComponent from './HeaderComponent.vue';
+import axios from 'axios';
 
 export default {
     name: "HomePage",
     data () {
         return {
-            name: ""
+            name: "",
+            restaurant: []
         }
     },      
     components: {
         HeaderComponent
     },
-    mounted() {
+    async mounted() {
         let user = localStorage.getItem("user-info");
         this.name = JSON.parse(user).name;
 
         if(!user) {
             this.$router.push({name: "SignUp"});
         }
+
+        let result = await axios.get("http://localhost:3000/restaurant");
+        this.restaurant = result.data;
     }
 }
 </script>
 
-<style></style>
+<style>
+    td {
+        width: 160px;
+        height: 40px;
+        text-align: center;
+    }
+</style>
